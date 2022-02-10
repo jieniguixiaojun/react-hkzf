ead225716a9a81e64249ba6ae09ca239b690eeee
jieniguixiaojun <94345559+jieniguixiaojun@users.noreply.github.com>
2022-02-10 22:27:53 +0800

    轮播图组件导入

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Carousel } from 'antd-mobile';
 
 export default class Index extends React.Component {
+    state = {
+        data: ['1', '2', '3'],
+    }
+    componentDidMount() {
+        // simulate img loading
+        setTimeout(() => {
+            this.setState({
+                data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            });
+        }, 100);
+    }
     render() {
-        return <div>这是首页</div>
+        return (
+            <div className="index">
+                <Carousel
+                    autoplay={false}
+                    infinite={false}
+                    autoplayInterval={1000}
+                >
+                    {this.state.data.map(val => (
+                        <a
+                            key={val}
+                            href="https://ys.mihoyo.com/main/manga"
+                            style={{
+                                display: 'inline-block',
+                                width: '100%',
+                                height: 212
+                            }}
+                        >
+                            <img
+                                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                alt=""
+                                style={{ width: '100%', verticalAlign: 'top' }}
+                            />
+                        </a>
+                    ))}
+                </Carousel>
+            </div>
+        );
     }
 }
