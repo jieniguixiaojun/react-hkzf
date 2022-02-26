9064d57d0ce500a06dddf658c83b216edb76700c
jieniguixiaojun <94345559+jieniguixiaojun@users.noreply.github.com>
2022-02-26 20:52:36 +0800

    获取轮播图数据

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,43 +1,53 @@
 import React from "react";
 import { Carousel } from 'antd-mobile';
 
+import axios from 'axios'
+import Item from "antd-mobile/lib/popover/Item";
+
 export default class Index extends React.Component {
     state = {
-        data: ['1', '2', '3'],
+        // 轮播图状态数据
+        swipers: []
+    }
+
+    // 获取轮播图数据的方法
+    async getSwipers() {
+        const res = await axios.get('http://localhost:8080/home/swiper')
+        this.setState(() => {
+            return {
+                swipers: res.data.body
+            }
+        })
     }
     componentDidMount() {
-        // simulate img loading
-        setTimeout(() => {
-            this.setState({
-                data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-            });
-        }, 100);
+        this.getSwipers()
+    }
+
+    // 渲染轮播图结构
+    renderSwiprs() {
+        return this.state.swipers.map(item => (
+            <a
+                key={Item.id}
+                href="https://itcast.cn"
+                style={{
+                    display: 'inline-block',
+                    width: '100%',
+                    height: 212
+                }}
+            >
+                <img
+                    src={`http://localhost:8080${item.imgSrc}`}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                />
+            </a>
+        ))
     }
     render() {
         return (
             <div className="index">
-                <Carousel
-                    autoplay={false}
-                    infinite={false}
-                    autoplayInterval={1000}
-                >
-                    {this.state.data.map(val => (
-                        <a
-                            key={val}
-                            href="https://ys.mihoyo.com/main/manga"
-                            style={{
-                                display: 'inline-block',
-                                width: '100%',
-                                height: 212
-                            }}
-                        >
-                            <img
-                                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                                alt=""
-                                style={{ width: '100%', verticalAlign: 'top' }}
-                            />
-                        </a>
-                    ))}
+                <Carousel autoplay infinite autoplayInterval={3000}>
+                    {this.renderSwiprs()}
                 </Carousel>
             </div>
         );
